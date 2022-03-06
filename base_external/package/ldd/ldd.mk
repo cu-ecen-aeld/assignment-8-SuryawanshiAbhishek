8f04b4e7a1c6cd8dac8463181fe3430ec7f41c18
SuryawanshiAbhishek <Abhishek.Suryawanshi@colorado.edu>
2022-03-06 14:09:08 -0700

    Assignment-7-part-2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,30 @@
+
+
+################################################################################
+#
+# ldd
+#
+################################################################################
+
+LDD_VERSION = e4212056d0c8c7bda90e36fc08a2a041ef16bdd8
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-SuryawanshiAbhishek.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = scull/
+LDD_MODULE_SUBDIRS += misc-modules/
+
+define LDD_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
